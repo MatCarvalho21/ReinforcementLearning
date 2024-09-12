f97008e959860f9f576befb1350c82ecb4927642
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2024-09-12 14:14:46 -0300

    esboço do funcionamento básico do jogo

--- checkers/game.py
+++ checkers/game.py
@@ -1,5 +1,4 @@
 import pygame
-from pieces import Piece
 
 # VARIÁVEIS GLOBAIS 
 FPS = 60
@@ -11,31 +10,39 @@ P2_COR_2 = (40, 69, 255)
 P1_COR_1 = (161, 23, 43)
 P1_COR_2 = (184, 54, 76)
 BACKGROUND = (255, 255, 255)
+RADIUS = 20
 
 class CheckersGame:
     def __init__(self, w=640, h=480):
+        """
+        
+        """
 
-        # INICIALIZANDO DISPLAY
         self.display_width = w
         self.display_height = h
         self.display = pygame.display.set_mode((self.display_width, self.display_height))
         pygame.display.set_caption('Checkers Game')
         self.clock = pygame.time.Clock()
         self.clock.tick(FPS)
-
-        # CONFIGURANDO JOGADOR 1
+        self.crown = pygame.image.load('checkers/assets/crown.png')
+        self.crown = pygame.transform.scale(self.crown, (30, 30))
+        self.shift = 2
         self.p1_npieces = 12
-        self.p1_pieces = [Piece(1, 1, (P1_COR_1, P1_COR_2), True), Piece(1, 3, (P1_COR_1, P1_COR_2), True), Piece(1, 5, (P1_COR_1, P1_COR_2)), Piece(1, 7, (P1_COR_1, P1_COR_2)),
-                          Piece(2, 2, (P1_COR_1, P1_COR_2)), Piece(2, 4, (P1_COR_1, P1_COR_2)), Piece(2, 6, (P1_COR_1, P1_COR_2)), Piece(2, 8, (P1_COR_1, P1_COR_2)),
-                          Piece(3, 1, (P1_COR_1, P1_COR_2)), Piece(3, 3, (P1_COR_1, P1_COR_2)), Piece(3, 5, (P1_COR_1, P1_COR_2)), Piece(3, 7, (P1_COR_1, P1_COR_2))]
-
-        # CONFIGURANDO JOGADOR 2
         self.p2_npieces = 12
-        self.p2_pieces = [Piece(6, 2, (P2_COR_1, P2_COR_2)), Piece(6, 4, (P2_COR_1, P2_COR_2)), Piece(6, 6, (P2_COR_1, P2_COR_2)), Piece(6, 8, (P2_COR_1, P2_COR_2)),
-                          Piece(7, 1, (P2_COR_1, P2_COR_2)), Piece(7, 3, (P2_COR_1, P2_COR_2)), Piece(7, 5, (P2_COR_1, P2_COR_2)), Piece(7, 7, (P2_COR_1, P2_COR_2)),
-                          Piece(8, 2, (P2_COR_1, P2_COR_2)), Piece(8, 4, (P2_COR_1, P2_COR_2)), Piece(8, 6, (P2_COR_1, P2_COR_2)), Piece(8, 8, (P2_COR_1, P2_COR_2))]
+
+        self.board = [1, 0, 1, 0, 1, 0, 1, 0,
+                      0, 3, 0, 1, 0, 1, 0, 1,
+                      1, 0, 1, 0, 1, 0, 1, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 2, 0, 2, 0, 2, 0, 2,
+                      2, 0, 4, 0, 2, 0, 2, 0,
+                      0, 2, 0, 2, 0, 2, 0, 2]
         
     def game_loop(self):
+        """
+        
+        """
         running = True
         while running:
             for event in pygame.event.get():
@@ -43,6 +50,10 @@ class CheckersGame:
                 if event.type == pygame.QUIT:
                     running = False
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self._move(mouse_pos)
+                   
             # Limpar a tela a cada frame
             self.display.fill(BACKGROUND)
 
@@ -55,11 +66,32 @@ class CheckersGame:
 
             if self.p1_npieces == 0 or self.p2_npieces == 0:
                 running = False
+                if self.p1_npieces == 0:
+                    print("Player 2 ganhou!")
+                else:
+                    print("Player 1 ganhou!")
 
         pygame.quit()
 
-    def _draw_board(self):
+    def _move(self, mouse_pos):
+        """
         
+        """
+
+        real_pos = (mouse_pos[1] - 40) // 50, (mouse_pos[0] - 120) // 50 # (X, Y)
+        piece = self.board[real_pos[0]*8 + real_pos[1]]
+
+        if piece == self.shift or piece == self.shift + 2:
+            print("OK")
+
+        else:
+            print("NOT OK")
+
+    def _draw_board(self):
+        """
+        
+        """
+
         pygame.draw.rect(self.display, COR_BORDA, (110, 30, 420, 420))
         pygame.draw.rect(self.display, COR_1, (120, 40, 400, 400))
 
@@ -69,8 +101,36 @@ class CheckersGame:
                     pygame.draw.rect(self.display, COR_2, (120 + j*50, 40 + i*50, 50, 50))
 
     def _draw_pieces(self):
-        for piece in self.p1_pieces + self.p2_pieces:
-            piece.draw(self.display)
+        """
+        
+        """
+
+        for each_pos in range(0, 64):
+
+            real_pos = (each_pos % 8, each_pos // 8)
+
+            if self.board[each_pos] == 1:
+
+                pygame.draw.circle(self.display, P1_COR_1, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS)
+                pygame.draw.circle(self.display, P1_COR_2, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS - 5)
+
+            elif self.board[each_pos] == 2:
+
+                pygame.draw.circle(self.display, P2_COR_1, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS)
+                pygame.draw.circle(self.display, P2_COR_2, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS - 5)
+
+            elif self.board[each_pos] == 3:
+
+                pygame.draw.circle(self.display, P1_COR_1, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS)
+                pygame.draw.circle(self.display, P1_COR_2, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS - 5)
+                self.display.blit(self.crown, (real_pos[0]*50 + 130, real_pos[1]*50 + 49))
+
+            elif self.board[each_pos] == 4:
+
+                pygame.draw.circle(self.display, P2_COR_1, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS)
+                pygame.draw.circle(self.display, P2_COR_2, (real_pos[0]*50 + 145, real_pos[1]*50 + 65), RADIUS - 5)
+                self.display.blit(self.crown, (real_pos[0]*50 + 130, real_pos[1]*50 + 49))
+
 
 if __name__ == '__main__':
     game = CheckersGame()
